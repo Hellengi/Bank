218b3a9f931bc353a5a04ede322f742e719c57ae
OikawaD <uranus.oj@gmail.com>
2025-05-05 16:35:27 +0300

    deploy

--- bank/settings.py
+++ bank/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +16,7 @@ SECRET_KEY = 'django-insecure-46q_46a3%f4x$s)$vjh(te@=q(xdp$q0jq2k&k)5(pr_4id5-(
 # DEBUG = os.environ.get('DEBUG') == 'True'
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -63,14 +64,21 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
 
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
